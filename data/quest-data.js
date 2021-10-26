54198056d16f38e2d7fe192818274f0a071b013f
Abriel Cleaver <abrielcleaver@gmail.com>
2021-10-26 16:56:09 -0700

    add quest-data file with template of quests

---
+++ data/quest-data.js
@@ -0,0 +1,70 @@
+// consts Shokugeki Arena, Polar Star Dormitory, Moon Banquet Festival, Stagiaire
+// id, title, map location, image, description, choices, hungry, full
+
+const shokugeki = {
+    id: 'shokugeki',
+    title: 'Welcome to the Shokugeki Arena',
+    // map: {},
+    // image: ,
+    // description: ,
+    // choices: [{
+       // id: '',
+        // description: '',
+        // result: '',
+        // hungry: ,
+        // full: ,
+   // }]
+};
+
+const dormitory = {
+    id: 'dormitory',
+    title: 'Welcome to the Polar Star Dormitory',
+    // map: {},
+    // image: ,
+    // description
+    // choices: [{
+        // id: '',
+        // description: '',
+        // result: '',
+        // hungry: ,
+        // full: ,
+   // }]
+};
+
+const moonFest = {
+    id: 'moon-festival',
+    title: 'Welcome to the Moon Banquet Festival',
+    // map: {},
+    // image: ,
+    // description
+    // choices: [{
+        // id: '',
+        // description: '',
+        // result: '',
+        // hungry: ,
+        // full: ,
+   // }]
+};
+
+const stagiaire = {
+    id: 'stagiaire',
+    title: 'Welcome to the Stagiaire',
+    // map: {},
+    // image: ,
+    // description:
+    // choices: [{
+       // id: '',
+        // description: '',
+        // result: '',
+        // hungry: ,
+        // full: ,
+   // }]
+};
+
+const quests = [
+    shokugeki, 
+    dormitory, 
+    moonFest,
+    stagiaire
+];
+export default quests;
