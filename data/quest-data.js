4255c1d879fd7a140a2ae9d136e7a7907e46d88e
Abriel Cleaver <abrielcleaver@gmail.com>
2021-10-27 02:41:22 -0700

    add description and main photo for quests

--- data/quest-data.js
+++ data/quest-data.js
@@ -3,10 +3,10 @@
 
 const shokugeki = {
     id: 'shokugeki',
-    title: 'Welcome to the Shokugeki Arena',
+    title: 'Shokugeki Arena',
     // map: {},
-    // image: ,
-    // description: ,
+    image: 'sns-Soma-vs-Ikumi-shokugeki-quest.png',
+    description: 'The Shokugeki between Mito Ikumi and Yukihira Soma has begun! Everyone, including Erina, is convinced that Ikumi, the Meat Master, will win. Soma, on the other hand, is greeted by jeers the moment he enters the arena. Will Soma be able to save Don RS?',
     // choices: [{
        // id: '',
         // description: '',
@@ -18,10 +18,10 @@ const shokugeki = {
 
 const dormitory = {
     id: 'dormitory',
-    title: 'Welcome to the Polar Star Dormitory',
+    title: 'Polar Star Dormitory',
     // map: {},
-    // image: ,
-    // description
+    image: 'sns-polar-star-quest.png',
+    description: 'The 1st years at the Totsuki Culinary Institute are facing the biggest event of the 2nd semester, “The Advancement Exam”! However, the mood is gloomy at Polar Star dormitory as it is rumored that those who won\'t obey Azami\'s regime will face elimination. As Soma reflects on the situation, he is visited by Erina. When he hears her thoughts, he starts to cook a “Yukihira dish” and asks her to taste! Will she like it?',
     // choices: [{
         // id: '',
         // description: '',
@@ -33,10 +33,10 @@ const dormitory = {
 
 const moonFest = {
     id: 'moon-festival',
-    title: 'Welcome to the Moon Banquet Festival',
+    title: 'Moon Banquet Festival',
     // map: {},
-    // image: ,
-    // description
+    image: 'sns-moonfestmain-questpage.jpeg' ,
+    description: 'The Moon Festival finally begins! The dish that Soma decides to make for his stand are black pepper pork buns that are made in a stone oven. However, the crowd keeps going over to the more luxurious Chinese cuisine RS\'s stand instead. Soma ends up in the red on the first day. Students who end up with losses during this school festival will be expelled, so Soma is already in trouble. Will he be able to turn things around?!',
     // choices: [{
         // id: '',
         // description: '',
@@ -48,10 +48,10 @@ const moonFest = {
 
 const stagiaire = {
     id: 'stagiaire',
-    title: 'Welcome to the Stagiaire',
+    title: 'The Stagiaire',
     // map: {},
-    // image: ,
-    // description:
+    image: 'sns-stagiaire-quest.png',
+    description: 'Following the Fall Elections, Soma\'s class is sent off to the Stagiare, a training program out in the real world where students must succeed in leaving visible results at their training location. Soma ends up at a Western restaurant with Arato. Can these two even work together?!',
     // choices: [{
        // id: '',
         // description: '',
