8608264c605057f63693741cd0756684a60d88f9
Abriel Cleaver <abrielcleaver@gmail.com>
2021-10-27 02:42:02 -0700

    add scoreQuest function

--- utils.js
+++ utils.js
@@ -18,3 +18,17 @@ export function getUser(){
     const userString = localStorage.getItem('USER');
     return JSON.parse(userString);
 }
+
+export function findById(id, items){
+    for (let item of items){
+        if (item.id === id){
+            return item;
+        }
+    }
+}
+
+export function scoreQuest(choiceObject, questId, userObject){
+    userObject.hungry += choiceObject.hungry;
+    userObject.full += choiceObject.full;
+    userObject.completed[questId] = true;
+}
