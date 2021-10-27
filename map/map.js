a13c217df8e4bb105dce89412226d488fe2ac1f7
Abriel Cleaver <abrielcleaver@gmail.com>
2021-10-26 18:25:39 -0700

    update map files

--- map/map.js
+++ map/map.js
@@ -1,1 +1,17 @@
 // console.log('hi from map page');
+
+// import quest data
+// import quests from '../data/quest-data.js'
+
+// console.log(quests);
+
+// const mapLinks = document.getElementById('map-links');
+
+// loop through each quest -- for (let quest of quests){};
+// create links using an <a> element for each quest with href = "/quest?id=<questId>"
+    // <a href="/quest?id=shokugeki>Shokugeki Battle</a>
+    // <a href="/quest?id=dormitory>Polar Star Dormitory</a>
+    // <a href="/quest?id=moon-festival>Moon Festival</a>
+    // <a href="/quest?id=stagiaire>Stagiaire</a>
+
+     // append link to map-links element
